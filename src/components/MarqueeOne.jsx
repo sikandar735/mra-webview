b52802c04436ea728c880f3904d6b5fc8a2967c0
Sakandar Hayat <91363027+sikandar735@users.noreply.github.com>
2024-07-18 10:37:23 +0500

    updated

--- src/components/MarqueeOne.jsx
+++ src/components/MarqueeOne.jsx
@@ -1,47 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
+import axios from "axios";
+
 const MarqueeOne = () => {
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get(
+          "https://mra-backend-api.vercel.app/api/news"
+        );
+        // Assuming the response.data is an array of news objects
+        setNewsData(response.data);
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    };
+
+    fetchNews();
+  }, []); // Empty dependency array ensures useEffect runs only once
+
   return (
     <div className="marquee-area bg-smoke">
       <div className="container-fluid p-0 overflow-hidden">
         <div className="slider__marquee">
           <div className="marquee_mode">
             <Marquee speed={50}>
-              <div className="item">
-               
-                  {/* <img src="assets/img/icon/marquee-icon-1-2.svg" alt="img" /> */}
-                  <i class="fa fa-drivers-license-o ml-2"></i>
-                  <span className="icon-text">Breaking Barriers</span>
-               
-              </div>
-              <div className="item">
-               
-                  {/* <img src="assets/img/icon/marquee-icon-1-1.svg" alt="img" /> */}
-                  <i class="fa fa-drivers-license-o ml-2"></i>
-                  <span className="icon-text"> New Park Opening in Downtown Milton</span>
-               
-              </div>
-              <div className="item">
-               
-                  {/* <img src="assets/img/icon/marquee-icon-1-2.svg" alt="img" /> */}
-                  <i class="fa fa-drivers-license-o ml-2"></i>
-                  <span className="icon-text"> Milton High School Achieves Record Exam Results</span>
-               
-              </div>
-              <div className="item">
-               
-                  {/* <img src="assets/img/icon/marquee-icon-1-1.svg" alt="img" /> */}
-                  <i class="fa fa-drivers-license-o ml-2"></i>
-                  <span className="icon-text"> Local  MRA Expo to be Held Next Month</span>
-               
-              </div>
-              <div className="item">
-               
-                  {/* <img src="assets/img/icon/marquee-icon-1-1.svg" alt="img" /> */}
-                  <i class="fa fa-drivers-license-o ml-2"></i>
-                  <span className="icon-text"> Milton Charity Run Raises Funds for Community Projects</span>
-               
-              </div>
+              {newsData.map((news) => (
+                <div key={news._id} className="item">
+                  <a href={news.url} target="_blank" rel="noopener noreferrer">
+                    {/* You can customize the icon or style here */}
+                    <i className="fa fa-drivers-license-o ml-2"></i>
+                    <span className="icon-text">{news.title}</span>
+                  </a>
+                </div>
+              ))}
             </Marquee>
           </div>
         </div>
